3bdc9b0bc173849706201bb4effb7613292c70fa
radolini3 <radolini3@o2.pl>
2022-05-01 21:09:38 +0200

    Działające DMA

---
+++ Core/Src/globalVars.h
@@ -0,0 +1,27 @@
+/*
+ * globalVars.h
+ *
+ *  Created on: Apr 30, 2022
+ *      Author: HP
+ */
+
+#ifndef SRC_GLOBALVARS_H_
+#define SRC_GLOBALVARS_H_
+
+	/*Zmienne przechowujące bufor dla przesyłania UART'em*/
+	extern char UartOutText[100], UartInput[5];
+	/*ADC zmienna*/
+
+	extern uint8_t Rh_byte1, Rh_byte2, Temp_byte1, Temp_byte2;
+	extern uint16_t SUM, RH, TEMP;
+	extern uint8_t Presence;
+
+	extern uint32_t ADC_VAL[7];
+	extern float moisture_percentage[6];
+
+	extern float Temperature;
+	extern float Humidity;
+	extern float lightIntensity;
+
+#endif /* SRC_GLOBALVARS_H_ */
+
